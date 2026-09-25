c652de5e35ad88e4800b73f6b453585010006b88
alan7reyes-lang <alan7reyes@gmail.com>
2026-09-24 18:27:44 -0700

    Add /buy redirect to buyer lead form

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
           "https://docs.google.com/forms/d/e/1FAIpQLSff8P__WNWYIvDiw20Ml1aVQd36nvBU1OaSDBrgoMrWPyYr_Q/viewform",
         permanent: false,
       },
+      {
+        source: "/buy",
+        destination:
+          "https://docs.google.com/forms/d/e/1FAIpQLSfyRsFVIStls6qVRLADA4OXRfsohJxVca1DGuouMA478H1xtg/viewform",
+        permanent: false,
+      },
     ]
   },
   images: {
